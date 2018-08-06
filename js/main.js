00add056e44e12f152b4e8a3d16d5fed8eac9ccb
Austin Du <akxdu@uwaterloo.ca>
2018-08-06 18:16:37 -0400

    added home page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $( "#search-restaurant" ).click(function() {
+      window.location.href = "restaurant_search.html";
+    });
+
+    $( "#create-restaurant" ).click(function() {
+      window.location.href = "basic_elements.html";
+    });
+});
