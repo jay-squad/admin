04c8bb37e83a70e8ab847d4d63c5dfea0fb69730
Yemin Shou <yeminlemon@hotmail.com>
2019-03-11 03:35:38 -0400

    aws keys from cookies

--- js/dishsubmissionview.js
+++ js/dishsubmissionview.js
@@ -29,8 +29,8 @@ $(document).ready(function() {
               endpoint: 'https://foodie-prod-dish-images.s3.amazonaws.com',
           },
           credentials: {
-              accessKey: 'AKIAIFYHWL2HA2CTAWFQ',
-              secretKey: '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',
+              accessKey: getCookie("aws_access_key"),
+              secretKey: getCookie("aws_secret_key"),
               expiration: d
           },
           signature: {
@@ -116,6 +116,22 @@ function appendForm(id) {
 
             return false;
       });
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = document.cookie;
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 function uuidv4() {
