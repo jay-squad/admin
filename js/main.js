1e64a158cb6f3b02eb22971f4eef27ce08fcab68
Yemin Shou <yeminlemon@hotmail.com>
2019-03-22 15:24:31 -0400

    remove fb stuff'

--- js/main.js
+++ js/main.js
@@ -12,37 +12,4 @@ $(document).ready(function() {
     });
 
     $.ajaxSetup({ cache: true });
-    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
-        FB.init({
-          appId: '2097330147147894',
-          version: 'v3.2' // or v2.1, v2.2, v2.3, ...
-        });
-        FB.getLoginStatus(statusChangeCallback);
-    });
 });
-
-function statusChangeCallback(response) {
-
-    if(response.status == 'connected') {
-        FB.api('/me', function(fb_me) {
-            $('#fb-status').text("FB Connected: " + fb_me.name);
-         });
-        $('#fb-login-button').hide();
-        setCookie("fb_access_token", response.authResponse.accessToken, response.authResponse.expiresIn)
-    } else {
-        $('#fb-login-button').show();
-    }
-}
-
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
-function setCookie(cname, cvalue, expire_secs) {
-    var d = new Date(Date.now() + expire_secs*1000);
-    var expires = "expires="+ d.toUTCString();
-    var cookie = cname + "=" + cvalue + ";" + expires + ";path=/;domain=foodie-server-prod.herokuapp.com;"
-    document.cookie = cookie;
-}
