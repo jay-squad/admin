7344f9d71a5d75ed8381689f99c4b9655f9c51ac
Yemin Shou <yeminlemon@hotmail.com>
2019-03-24 18:59:07 -0400

    create conf.js lol

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var fs = require("fs");
+var data = "const FOODIE_SERVER_BASE = \"https://foodie-server-prod.herokuapp.com\"";
+
+fs.writeFile("js/conf.js", data, function(err, data) {
+  if (err) console.log(err);
+  console.log("Successfully Written conf.js");
+});
+
 express()
   .use(express.static(__dirname))
   .use(express.static(path.join(__dirname, 'pages/forms')))
